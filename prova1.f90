7c2d38881a8a761bcd8dd520724eb4b722c12369
Paolo Posocco <posocco@gmail.com>
2013-03-30 19:26:11 +0100

    prova invio f90

--- prova1.f90
+++ prova1.f90
@@ -1,1 +1,7 @@
 !prima prova
+double precision function y_lorentz()
+      
+
+! TO DO
+      !una volta fatto cerca libreria che lo faccia x confronto
+      !eventualment subroutine parser x leggere da dalton
