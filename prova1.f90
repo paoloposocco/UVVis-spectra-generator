921de5f2425c47c298190a0bf2ee432ab5a46665
Paolo Posocco <posocco@gmail.com>
2013-04-03 18:07:10 +0200

    aggiunta versione rudimentale di parser che legge tre righe dal file input.txt

--- prova1.f90
+++ prova1.f90
@@ -22,16 +22,31 @@ real function y_lorentz2(x_current, x_center, fwhm, height)
 program UV_fort
       implicit none
       integer::n, n_max, errore_io, step_number, i, j
+      integer::allocate_status, lines
       real::min_window, max_window, step_width, y_threshold
       real:: x_current, y_current, y_lorentz, y_lorentz2
       !real array allocabile x lo spettro
       ! real array di input
       real, dimension(100:350,2):: spectrum
-      real, dimension(1:3,1:2):: input
+      real, dimension(:,:), allocatable:: input
       !input=(/(5.276733, 0.1299), (6.000544, 0.0321), (6.667589, 0.1031)/)
       !input=(/(5.276733, 6.000544, 6.667589), (0.1299, 0.0321, 0.1031)/)
       ! se mi accontento della precisione intera posso usare lo stesso valore
       ! come indice per x_center
+      OPEN(UNIT=5 , FILE ='spectrum.out',STATUS='REPLACE', ACTION='WRITE',&
+              &iostat=errore_io)
+      OPEN(UNIT=3 , FILE ='input.txt',STATUS='old', ACTION='read',&
+              &iostat=errore_io)
+      write(*,*) 'scrivi numero linee input'
+      !read(*,*) lines
+      lines=3
+      allocate (input(lines,2), stat= allocate_status)
+      do i=1, lines
+            read(3,*) input(i,1),input(i,2)
+              write(*,*) 'input= ', input
+            end do
+      if (allocate_status/=0) STOP 'ERRORE ALLOCAZIONE MEMORIA'
+      write(*,*) 'input= ', input
       input(1,1)=235
       input(1,2)=0.1299
       !input(1,2)=26
@@ -41,17 +56,14 @@ program UV_fort
       input(3,1)=186
       input(3,2)=0.1031
       !input(3,2)=2.06
+      write(*,*) 'input= ', input
       spectrum=0.0
       do i=350, 100, -1
             spectrum(i,1) = i
             end do 
-      write(*,*) 'spectrum= ', spectrum
-      OPEN(UNIT=5 , FILE ='spectrum.out',STATUS='REPLACE', ACTION='WRITE',&
-              &iostat=errore_io)
-      if(errore_io /= 0) then
-            write(*,*) 'errore output' ! poi vedere se mettere anche controllo
-            ! su input
-      else
+      !write(*,*) 'spectrum= ', spectrum
+      if(errore_io /= 0) stop 'errore scrittura file' !v. se controllare anche
+      ! input
             write(*,*) 'tutot ok'
       do j=1,3
             do i=100, 350
@@ -61,8 +73,5 @@ program UV_fort
       do i=100,350
                     write(5,*) spectrum(i,:)
                     end do
-      end if
-
-      !write(5,'(2E10.3)') spectrum
       stop
       end program UV_fort
